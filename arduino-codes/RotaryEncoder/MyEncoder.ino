610a3a1cd06ed4ed9fa062d4e14d413e173964c9
Laris Qiao <laris.qiao@gmail.com>
2018-06-17 23:27:20 +0800

    Revert "fix email"
    This reverts commit 6234bd355885b26f1659c6900a5b093d5cfa1b2e.

--- arduino-codes/RotaryEncoder/MyEncoder.ino
+++ arduino-codes/RotaryEncoder/MyEncoder.ino
@@ -18,7 +18,7 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 
-	Questions?  Send mail to mschwage_AT_gmail.com
+	Questions?  Send mail to mschwage@gmail.com
 
 */
 
